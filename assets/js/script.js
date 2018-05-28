055025ef3bdf78bf21997cac9ab1f9321ed05315
Paulo Alves <front@agencianovi.com.br>
2018-05-28 16:04:43 -0300

    Finalizaçao

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,4 +35,49 @@
             }
         }
     });
+    $( document ).ready(function(){
+        $('.now').css("width", function(){
+            return $(this).attr("data")
+        })
+    });
+    const second = 1000,
+    minute = second * 60,
+    hour = minute * 60,
+    day = hour * 24;
+
+    let countDown = new Date('May 31, 2019 16:00:00').getTime(),
+    x = setInterval(function() {
+
+        let now = new Date().getTime(),
+        distance = countDown - now;
+
+        // document.getElementById('days').innerText = Math.floor(distance / (day)),
+        document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
+        document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute));
+        // document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second)
+    }, second)
+    $(".timer").waypoint(function(){
+        $(this).countTo();
+    },{
+        triggerOnce: true,
+        offset: "bottom-in-view"
+    });
+
+    $(document).ready(function() {
+        var readURL = function(input) {
+            if (input.files && input.files[0]) {
+                var reader = new FileReader();
+                reader.onload = function (e) {
+                    $('.profile-pic').attr('src', e.target.result);
+                }
+                reader.readAsDataURL(input.files[0]);
+            }
+        }
+        $(".file-upload").on('change', function(){
+            readURL(this);
+        });
+        $(".upload-button").on('click', function() {
+            $(".file-upload").click();
+        });
+    });
 })(jQuery, window);
